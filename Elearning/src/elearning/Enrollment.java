e75210e8af3112d3c6e58e19ce0bbc636f6798c7
EfrataMillion <efratamillion@gmail.com>
2019-12-12 01:48:17 -0800

    Add files via upload

---
+++ Elearning/src/elearning/Enrollment.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elearning;
+
+/**
+ *
+ * @author kirubel
+ */
+public class Enrollment {
+    private String dateOfEnrollment;
+    public Enrollment(){
+    }
+    
+    public void enroll(){
+        System.out.println("Enrolled!!");
+    }
+    
+     public void viewEnrollment(){
+        System.out.println("View Enrollement!!");
+    }
+}
