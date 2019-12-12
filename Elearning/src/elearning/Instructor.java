24ea76a4d8c254e15df01849c5f6e316efc9faa6
YG-brish <37708265+YG-brish@users.noreply.github.com>
2019-12-12 12:57:57 +0300

    Add files via upload

---
+++ Elearning/src/elearning/Instructor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elearning;
+
+/**
+ *
+ * @author kirubel
+ */
+public class Instructor extends Authentication {
+    
+    private String fullName;
+    private String instructorId;
+    private String worksIn;
+    private String Bio;
+    private String level;
+    public Instructor(){
+        
+    }
+    
+    
+    
+}
