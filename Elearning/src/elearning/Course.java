94d390bc5f1ea385a0240665eeb32614dbc7bdaa
Kirubel19 <kirubeltesfaye19@gmail.com>
2019-12-12 03:01:02 -0600

    Add files via upload

---
+++ Elearning/src/elearning/Course.java
@@ -0,0 +1,47 @@
+package elearning;
+
+import .*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kirubel
+ */
+public class Course {
+    private Integer courseId;
+    private String courseName;
+    private String courseDescription;
+    private String courseCatagory;
+    private String photoUrl;
+    private String level;
+    
+    public Course(){
+    }
+    public void createCourse(){
+        System.out.println("Course Created!");
+    }
+      public void updateCourse(){
+        System.out.println("Course Updated!");
+    }
+       public void activateCourse(){
+        System.out.println("Course Activated!");
+    }
+        public void deactivateCourse(){
+        System.out.println("Course Deactivated!");
+    }
+        public void viewCourse(){
+            System.out.println("Course viewed");
+        }
+       public void searchCourse(){
+            System.out.println("Search result....");
+        }
+       
+      
+      
+    
+}
